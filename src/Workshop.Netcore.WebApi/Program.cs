e32d14e1825c854a9130ca54dc5c35c5feb67243
Bruno Abude <brunoabude@outlook.com>
2024-06-16 16:20:44 -0300

    Capítulo 6
    Dockerfile

--- src/Workshop.Netcore.WebApi/Program.cs
+++ src/Workshop.Netcore.WebApi/Program.cs
@@ -21,11 +21,11 @@ builder.Services.AddIdentityApiEndpoints<WebApiUser>()
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
